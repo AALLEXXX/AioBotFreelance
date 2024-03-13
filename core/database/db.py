82d09d0a30a576414e81e774af2617661e41db2d
Alex <alxjy@ya.ru>
2024-03-14 00:17:21 +0300

    some refactor

--- core/database/db.py
+++ core/database/db.py
@@ -1,21 +1,12 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, AsyncAttrs
-from sqlalchemy.orm import  sessionmaker, DeclarativeBase 
-import asyncio
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from settings import settings
-
-
 
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
-async_session_maker = sessionmaker(engine, class_=AsyncSession,expire_on_commit=False)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
-    pass 
-
-
-
-# async def async_main():
-#     async with engine.begin() as session: 
-#         await session.run_sync(Base.metadata.create_all)
+    pass
