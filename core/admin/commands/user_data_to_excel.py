67e074c1d836b73f603c84d456f03731fce05321
Alex <alxjy@ya.ru>
2024-03-13 18:05:58 +0300

    refactor and add docstring for func make_excel_users_file

--- core/admin/commands/user_data_to_excel.py
+++ core/admin/commands/user_data_to_excel.py
@@ -1,4 +1,3 @@
-from aiogram import Bot
 from aiogram.types import Message
 from core.database.requests import UserDAO
 import openpyxl
@@ -9,10 +8,13 @@ from aiogram.types import FSInputFile
 
 
 async def make_excel_users_file() -> str:
+    """
+    making excel file and return file name 
+    """
     users_models: list = await UserDAO.get_all_by_params()
     users = [[i.chat_id, i.fullname, i.role, 
               i.phone_number, 
-              i.date_registration.strftime("%Y-%m-%d %H:%M:%S") 
+              i.date_registration.strftime('%Y-%m-%d %H:%M:%S') 
               if i.date_registration else i.date_registration, i.tg_username] 
               for i in users_models]
     
@@ -23,17 +25,17 @@ async def make_excel_users_file() -> str:
     for row_data in users:
         sheet.append(row_data)
 
-    time = datetime.now().time().strftime("%H:%M:%S")
+    time = datetime.now().time().strftime('%H:%M:%S')
     file_name = f'users {datetime.now().date()}_{time}.xlsx'
-    wb.save(f"core/users_excel_files/{file_name}")
+    wb.save(f'core/users_excel_files/{file_name}')
     return file_name
 
 
 # command - /get_users_data 
-async def get_excel_usersdata_file(msg: Message, bot: Bot):
-    file_name = await make_excel_users_file()
+async def get_excel_usersdata_file(msg: Message) -> Message:
+    file_name: str = await make_excel_users_file()
 
-    file_path = f'core/users_excel_files/{file_name}'
+    file_path: str = f'core/users_excel_files/{file_name}'
 
-    await  msg.reply_document(document=FSInputFile(path=file_path),)
+    return await msg.reply_document(document=FSInputFile(path=file_path),)
 
