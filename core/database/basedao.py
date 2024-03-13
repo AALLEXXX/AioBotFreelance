31c30bebfb92e8a31eb33a346d42683099891ac2
Alex <alxjy@ya.ru>
2024-03-13 18:11:33 +0300

    refactor

--- core/database/basedao.py
+++ core/database/basedao.py
@@ -1,5 +1,6 @@
+from typing import Any
 from core.database.db import async_session_maker
-from sqlalchemy import Result, ResultProxy, insert, select, update, and_
+from sqlalchemy import ResultProxy, RowMapping, insert, select, update, and_
 
 
 class BaseDAO:
@@ -13,21 +14,21 @@ class BaseDAO:
 
 
     @classmethod
-    async def find_by_id(cls, model_id: int):
+    async def find_by_id(cls, model_id: int) -> RowMapping | Any | None:
         async with async_session_maker() as session: 
             query = select(cls.model.__table__.columns).filter(cls.model.id == model_id)
             result: ResultProxy = await session.execute(query)
             return result.mappings().one_or_none()
         
     @classmethod
-    async def add(cls, **data):
+    async def add(cls, **data) -> None:
         async with async_session_maker() as session: 
             query = insert(cls.model.__table__).values(**data)
             await session.execute(query)
             await session.commit()
 
     @classmethod
-    async def update_by_id(cls, model_id, **data):
+    async def update_by_id(cls, model_id, **data) -> None:
         async with async_session_maker() as session:
             condition = cls.model.__table__.c.id == model_id
             query = update(cls.model.__table__).where(condition).values(**data)
--- core/filters/isadmin.py
+++ core/filters/isadmin.py
@@ -6,7 +6,7 @@ from core.database.requests import UserDAO
 
 class IsAdmin(Filter):
     async def __call__(self, msg: Message) -> bool:
-        chat_id = msg.from_user.id
+        chat_id: int = msg.from_user.id
         admins: List[Tuple[str,]] = await UserDAO.get_chat_id_all_admin()
         if any(chat_id_tuple[0] == chat_id for chat_id_tuple in admins):
             return True
