c36ba876ce2542887282025deb4b4ee1a8d86f41
Alex <alxjy@ya.ru>
2024-03-14 17:44:25 +0300

    refactor

--- core/admin/commands/user_data_to_excel.py
+++ core/admin/commands/user_data_to_excel.py
@@ -7,24 +7,40 @@ from core.database.requests import UserDAO
 from core.excel.excel_utility import make_excel_file
 from core.database.models import User
 
-# command - /get_users_data 
+
+# command - /get_users_data
 async def get_excel_usersdata_file(msg: Message) -> Message:
     users_models: list = await UserDAO.get_all_by_params()
-    
-    users:list[list] = [[i.chat_id, i.fullname, i.role, 
-              i.phone_number, 
-              i.date_registration.strftime('%Y-%m-%d %H:%M:%S') 
-              if i.date_registration else i.date_registration, i.tg_username] 
-              for i in users_models]
-    
+
+    users: list[list] = [
+        [
+            i.chat_id,
+            i.first_name,
+            i.last_name,
+            i.middle_name,
+            i.role,
+            i.phone_number,
+            (
+                i.date_registration.strftime("%Y-%m-%d %H:%M:%S")
+                if i.date_registration
+                else i.date_registration
+            ),
+            i.tg_username,
+        ]
+        for i in users_models
+    ]
+
     columns_names: List[str] = User().__table__.columns.keys()
 
-    time = datetime.now().time().strftime('%H:%M:%S')
-    file_name = f'users {datetime.now().date()}_{time}.xlsx'
-    file_path: str = 'core/excel/users_excel_files/'
+    time: str = datetime.now().replace(microsecond=0)
+    file_name: str = f"users {datetime.now().date()}_{time}.xlsx"
+    file_path: str = "core/excel/users_excel_files/"
 
-
-    await make_excel_file(users, columns_names, file_path, file_name)
+    await make_excel_file(
+        data_list=users,
+        newfile_path=file_path,
+        newfile_name=file_name,
+        names_colums=columns_names,
+    )
 
     return await msg.reply_document(document=FSInputFile(path=file_path + file_name))
-
