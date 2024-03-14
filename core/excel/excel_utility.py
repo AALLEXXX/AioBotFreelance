93800d593c9ad752887b94f6dc8682c85c47ac6a
Alex <alxjy@ya.ru>
2024-03-14 17:53:15 +0300

    change args names

--- core/excel/excel_utility.py
+++ core/excel/excel_utility.py
@@ -3,26 +3,26 @@ import openpyxl
 import os
 
 
-async def make_excel_file(data_list: list[list],
-                           names_tables: List[str],
-                            newfile_path: str, 
-                            newfile_name: str
-                             ) -> str:
+async def make_excel_file(
+    data_list: list[list],
+    newfile_path: str,
+    newfile_name: str,
+    names_colums: List[str] = None,
+) -> str:
     """
     making new excel file and delete old file
     """
-    
 
     files_in_directory = os.listdir(newfile_path)
 
-    
     for filename in files_in_directory:
         filepath = os.path.join(newfile_path, filename)
         os.remove(filepath)
-    
+
     wb = openpyxl.Workbook()
     sheet = wb.active
-    sheet.append(names_tables)
+    if names_colums:
+        sheet.append(names_colums)
     for row_data in data_list:
         sheet.append(row_data)
     wb.save(newfile_path + newfile_name)
