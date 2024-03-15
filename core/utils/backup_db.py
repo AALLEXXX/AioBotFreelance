074f95317f4446c20b67b48dc91c0de2b8d621e1
Alex <alxjy@ya.ru>
2024-03-15 17:37:01 +0300

    added backup db

--- core/utils/backup_db.py
+++ core/utils/backup_db.py
@@ -13,4 +13,4 @@ async def send_mes(bot: Bot, chat_id : int, path: str):
 async def schedule_backup_db_file(apscheduler: AsyncIOScheduler):
     apscheduler.add_job(send_mes, trigger='cron', 
                         hour='23', minute='59', second='0',
-                        kwargs={'chat_id': settings.ADMIN_ID, 'path': r"core/database/TgBotDb.sqlite3"})git 
+                        kwargs={'chat_id': settings.ADMIN_ID, 'path': r"core/database/TgBotDb.sqlite3"})
